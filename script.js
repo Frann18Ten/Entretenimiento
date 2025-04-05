fcf9a48844d88ca2ee2219e7d8226dcecd4d94d1
Frann18Diez <tiendagsservice@gmail.com>
2025-04-05 02:05:58 -0300

    Agrego script para filtros y modal

---
+++ script.js
@@ -0,0 +1,21 @@
+function filterContent(category) {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => {
+    if (category === 'all' || card.classList.contains(category)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
+
+function openModal(src) {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  modal.style.display = "block";
+  modalImg.src = src;
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
